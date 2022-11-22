f778c5fc6060eb80601aa66015dda771166e48c2
Obsessive-Coder <jaredhuff85@gmail.com>
2022-11-22 06:36:41 -0600

    Image Upload
    * Add AWS S3.
    * Connect to AWS S3 Bucket.
    * Upload file to AWS S3.
    * Send image url back to client.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,13 @@ const categoryTypeRouter = require('./CategoryType.Router');
 const orderRouter = require('./Order.Router');
 const orderStatusRouter = require('./OrderStatus.Router');
 const productRouter = require('./Product.Router');
+const fileRouter = require('./File.Router');
 
 router.use('/api/categories', categoryRouter);
 router.use('/api/category-types', categoryTypeRouter);
 router.use('/api/orders', orderRouter);
 router.use('/api/order-statuses', orderStatusRouter);
 router.use('/api/products', productRouter);
+router.use('/api/files', fileRouter);
 
 module.exports = router;
