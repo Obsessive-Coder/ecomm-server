58504b47e5bc3ffac57b6e1bf798adafbe1982b6
Obsessive-Coder <jaredhuff85@gmail.com>
2022-12-07 13:21:13 -0600

    find one order

--- app/controllers/Order.Controller.js
+++ app/controllers/Order.Controller.js
@@ -36,6 +36,43 @@ class OrderController extends GenericController {
       })
       .catch(error => res.status(500).send(this.handleError(error)));
   }
+
+  findOne(req, res) {
+    const { id } = req.params;
+
+    this.TableModel.findOne({
+      where: { id },
+      include: [{
+        model: db.OrderItem,
+        include: [{
+          model: db.Product,
+          attributes: ['title']
+        }]
+      }, {
+        model: db.OrderStatus,
+        attributes: ['title', 'description']
+      }]
+    })
+      .then(record => {
+        const { id, updatedAt, address, phone, payment, status_id, OrderItems, OrderStatus } = record;
+
+        res.send({
+          id,
+          date: updatedAt,
+          address,
+          phone,
+          payment,
+          status_id,
+          orderItems: OrderItems.map((item) => ({
+            ...item.dataValues,
+            title: item.Product.title
+          })),
+          total: OrderItems.reduce((prev, { item_price }) => prev + item_price, 0),
+          status: OrderStatus.title
+        });
+      })
+      .catch(error => res.status(500).send(this.handleError(error)));
+  }
 }
 
 module.exports = new OrderController();
