9fed36176d42c24cc78bb84c9cef6db581dbcb82
Jared Huffstutler <jaredhuff85@gmail.com>
2023-01-16 04:58:55 -0600

    Update product quantity wihen an item is deleted

--- app/controllers/OrderItem.Controller.js
+++ app/controllers/OrderItem.Controller.js
@@ -20,12 +20,35 @@ class OrderItemController extends GenericController {
     const { id, product_id, quantity: newQuantity } = req.body;
 
     db.OrderItem.findByPk(id, { attributes: ['quantity'] })
-      .then(({ quantity: previousQuantity }) => newQuantity - previousQuantity)
+      .then((orderItem) => {
+        if (orderItem) {
+          const { quantity: previousQuantity } = orderItem;
+          return newQuantity - previousQuantity
+        } else {
+          return 0;
+        }
+      })
       .then(quantityDifference => {
         db.Product.findByPk(product_id, { attributes: ['quantity'] })
           .then(({ quantity: productQuantity }) => productQuantity - quantityDifference)
           .then(quantity => db.Product.update({ quantity }, { where: { id: product_id } }))
           .then(() => super.update(req, res));
+      })
+  }
+
+  destroy(req, res) {
+    const { id } = req.params;
+
+    // Get order item quantity.
+    // Get product quantity.
+    // Update product quantity.
+
+    db.OrderItem.findByPk(id, { attributes: ['product_id', 'quantity'] })
+      .then(({ product_id, quantity: itemQuantity }) => {
+        db.Product.findByPk(product_id, { attributes: ['quantity'] })
+          .then(({ quantity: productQuantity }) => productQuantity += itemQuantity)
+          .then(quantity => db.Product.update({ quantity }, { where: { id: product_id } }))
+          .then(() => super.destroy(req, res));
       })
   }
 }
