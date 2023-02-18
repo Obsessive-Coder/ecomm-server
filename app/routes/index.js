ff9701c96e6a0d9ce2d1749d8d616fff5bdfb460
Jared Huffstutler <jaredhuff85@gmail.com>
2023-02-18 08:01:29 -0600

    Metrics
    * Add router for metrics.
    * Add controller for metrics.

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,7 @@ const orderItemRouter = require('./OrderItem.Router');
 const orderStatusRouter = require('./OrderStatus.Router');
 const productRouter = require('./Product.Router');
 const fileRouter = require('./File.Router');
+const metricRouter = require('./Metric.Router');
 
 router.use('/api/categories', categoryRouter);
 router.use('/api/category-types', categoryTypeRouter);
@@ -14,5 +15,6 @@ router.use('/api/order-items', orderItemRouter);
 router.use('/api/order-statuses', orderStatusRouter);
 router.use('/api/products', productRouter);
 router.use('/api/files', fileRouter);
+router.use('/api/metrics', metricRouter);
 
 module.exports = router;
